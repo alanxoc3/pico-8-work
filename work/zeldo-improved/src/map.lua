c5f2af34f28df72aaf4b606b8599a07f8a5b67ec
Alan Morgan <alanxoc3@gmail.com>
2019-01-20 18:42:01 -0700

    Working on connecting caves/houses.

--- work/zeldo-improved/src/map.lua
+++ work/zeldo-improved/src/map.lua
@@ -46,7 +46,7 @@ function map_init()
       dun71={c=3, x=104, y=56, w=8, h=8, u={"dun70",4,8}, d={"dun70",4,0}, r={"dun70",0,4}, l={"dun70",8,4}},
       dun72={c=3, x=112, y=56, w=8, h=8, u={"dun62",4,8}, d={"dun71",4,0}, r={"dun73",0,4}, l={"dun71",8,4}},
       dun73={c=3, x=120, y=56, w=8, h=8, u={"bossw",8,12}, l={"dun72",8,4}},
-      villa={c=3, x=29,  y=0,  w=22, h=25, r={"field",0,26}, u={"lankp",5,20}},
+      villa={c=3, x=29,  y=0,  w=22, h=25, r={"field",0,26}, u={"lankp",5,20}, doors={{"dun20",4,8,18.5,2.5,.5,.5}, {"shop",4,8,4,11.5,.5,.5}, {"banjo_academy",4,8,17,19.5,.5,.5}}},
       field={c=3, x=0,   y=0,  w=29, h=32, l={"villa",22,13}, r={"gravp",0,3}, u={"mount",7,25}},
       bossw={c=3, x=64,  y=12, w=16, h=12, d={"dun73",4,0}, u={"dun63",4,8}},
       lankp={c=3, x=32,  y=32, w=10, h=20, d={"villa",4,0}},
@@ -54,18 +54,19 @@ function map_init()
       grave={c=5, x=48,  y=25, w=16, h=21, l={"gravp",19,4}},
       mount={c=4, x=51,  y=0,  w=13, h=25, d={"field",12,0}, u={"castl",8,12}}, -- mtn
       castl={c=5, x=32,  y=52, w=16, h=12, d={"mount",6,0}},
-      templ={c=5, x=64,  y=0,  w=16, h=12, d={"gravp",14,0}},
+      templ={c=5, x=64,  y=0,  w=16, h=12, d={"gravp",11,0}},
 
-      room0={c=4, x=54, y=46, w=5,  h=6 }, -- xs room 1
-      room1={c=4, x=59, y=46, w=5,  h=6 }, -- xs room 2
-      room2={c=4, x=42, y=46, w=6,  h=6 }, -- small room 1
-      room3={c=4, x=48, y=46, w=6,  h=6 }, -- small room 2
-      room4={c=4, x=42, y=32, w=6,  h=7 }, -- medium room 1
-      room5={c=4, x=42, y=39, w=6,  h=7 }, -- medium room 2
-      room6={c=4, x=64, y=24, w=8,  h=8 }, -- big room 1
-      room7={c=4, x=72, y=24, w=8,  h=8 }, -- big room 2
-      room8={c=4, x=80, y=24, w=8,  h=8 }, -- big room 3
-      room9={c=4, x=88, y=24, w=8,  h=8 }, -- big room 4
+      novi_grave={c=4, x=54, y=46, w=5,  h=6 }, -- xs room 1
+      ivan_grave={c=4, x=59, y=46, w=5,  h=6 }, -- xs room 2
+      house_1={c=4, x=42, y=46, w=6,  h=6 }, -- small room 1
+      house_2={c=4, x=48, y=46, w=6,  h=6 }, -- small room 2
+      house_3={c=4, x=42, y=32, w=6,  h=7 }, -- medium room 1
+      house_4={c=4, x=42, y=39, w=6,  h=7 }, -- medium room 2
+      lanks_house={c=4, x=64, y=24, w=8,  h=8 }, -- big room 1
+      shop={c=4, x=72, y=24, w=8,  h=8, d={"villa",4,12} }, -- big room 2
+      banjo_academy={c=4, x=80, y=24, w=8,  h=8, d={"villa",17,20}}, -- big room 3
+      gravekeepers_house={c=4, x=88, y=24, w=8,  h=8}, -- big room 4
+
       bossi={c=0, x=48, y=52, w=16, h=12}, -- final hard ivan battle
       bossc={c=5, x=80, y=0,  w=16, h=12}, -- cannon boss
       endls={c=3, x=80, y=12, w=16, h=12}, -- endless mode
@@ -91,6 +92,14 @@ end
 
 function room_update()
    cur_room = g_rooms[g_cur_room]
+
+   -- "room", out_x, out_y, x, y, rx, ry
+   foreach(cur_room.doors or {}, function(d)
+      if point_in_rect(g_pl, {x=g_rx+d[4], y=g_ry+d[5], rx=d[6], ry=d[7]}) then
+         load_room(d[1], d[2], d[3])
+      end
+   end)
+
    if g_pl.y > g_ry+g_rh and cur_room.d then
       load_room(cur_room.d[1], cur_room.d[2], cur_room.d[3])
    elseif g_pl.y < g_ry and cur_room.u then
