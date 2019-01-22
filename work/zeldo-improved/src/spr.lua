6fd3ca3813ea5ee20f66d3d1afada8e2c2eb23e4
Alan Morgan <alanxoc3@gmail.com>
2019-01-22 14:44:39 -0700

    working on outlining

--- work/zeldo-improved/src/spr.lua
+++ work/zeldo-improved/src/spr.lua
@@ -3,7 +3,7 @@ function restore_pal()
 end
 
 -- this is zeldo specific
--- function scr_spr10(a)
+-- - -function scr_spr10(a)
    -- sspr(
       -- 48+a.sind%8*10,
       -- 24+flr(a.sind/8)*10,
@@ -16,93 +16,58 @@ end
    -- )
 -- end
 
-function scr_spr(a)
-   spr(a.sind, scr_x(a.x-a.sw*.5)+a.xx, scr_y(a.y-a.sh*.5)+a.yy, a.sw, a.sh, a.xf, a.yf)
+function scr_spr(a, spr_func)
+   (spr_func or spr)(a.sind, scr_x(a.x-a.sw*.5)+a.xx, scr_y(a.y-a.sh*.5)+a.yy, a.sw, a.sh, a.xf, a.yf, 1)
 end
 
-function outline_sprite(a)
-   -- reset palette to black
-   for c=1,15 do
-      pal(c,col_outline)
-   end
-   -- draw outline
-   for xx=-1,1 do
-      for yy=-1,1 do
-         spr(a.sind, scr_x(a.x-a.sw*.5)+a.xx+xx, scr_y(a.y-a.sh*.5)+a.yy+yy, a.sw, a.sh, a.xf, a.yf)
+function scr_spr_out(a) scr_spr(a, spr_out) end
+
+g_out_cache = {}
+function init_out_cache(s_beg, s_end)
+   for sind=s_beg,s_end do
+      local bounds, is_bkgd = {}, function(x, y)
+         return mid(0,x,7) == x and sget(x+sind*8%128, y+flr(sind/16)*8) != 0
       end
-   end
-   -- reset palette
-   pal()
-   -- draw final sprite
-   spr(a.sind, scr_x(a.x-a.sw*.5)+a.xx, scr_y(a.y-a.sh*.5)+a.yy, a.sw, a.sh, a.xf, a.yf)
-end
 
-g_spr_out_cache = {}
-function gen_sprite_outline(a)
-   -- {10, 10, 20, 20, 1},
-   -- {10, 10, 20, 20, 1},
-   -- {10, 10, 20, 20, 1},
-   -- {10, 10, 20, 20, 1},
-   -- {10, 10, 20, 20, 1},
-   -- {10, 10, 20, 20, 1},
-   local sind = a.sind
-   if not g_spr_out_cache[sind] then
-      local sx, sy = (sind % 16)*8, (flr(sind / 16))*8
-      g_spr_out_cache[sind] = {}
-      local pre_out = {}
+      local calc_bound = function(x)
+         local top, bot
 
-      for col=0,7 do
-         local top, bot = 0, 0
          for i=0,7 do
-            if sget(sx+col,sy+i) != 0 then
-               top = i-1
-               break
-            end
+            top, bot = top or is_bkgd(x,i) and i-1, bot or is_bkgd(x,7-i) and 8-i
          end
 
-         for i=7,0,-1 do
-            if sget(sx+col,sy+i) != 0 then
-               bot = i+1
-               break
-            end
-         end
-
-         add(pre_out, {top, bot})
+         return {top=top or 10, bot=bot or 0}
       end
 
-      pre_out[0] = {10, 0}
-      pre_out[9] = {10, 0}
-      for i=1,8 do
-         local new_out = {}
+      g_out_cache[sind] = {}
+      for i=0xffff,8 do
+         -- prev, cur, next
+         local p, c, n = calc_bound(i-1), calc_bound(i), calc_bound(i+1)
+         local top, bot = min(min(p.top, c.top), n.top), max(max(p.bot, c.bot), n.bot)
 
-         new_out[1] = min(min(pre_out[i-1][1], pre_out[i][1]), pre_out[i+1][1])
-         new_out[2] = max(max(pre_out[i-1][2], pre_out[i][2]), pre_out[i+1][2])
-
-         add(g_spr_out_cache[sind], {i-1,new_out[1],i-1,new_out[2],1})
-
-         if i == 1 then
-            new_out[1] = min(pre_out[i-1][1], pre_out[i][1])
-            new_out[2] = max(pre_out[i-1][2], pre_out[i][2])
-            add(g_spr_out_cache[sind], {-1,new_out[1],-1,new_out[2],1})
-         end
-
-         if i == 8 then
-            new_out[1] = min(pre_out[i+1][1], pre_out[i][1])
-            new_out[2] = max(pre_out[i+1][2], pre_out[i][2])
-            add(g_spr_out_cache[sind], {8,new_out[1],8,new_out[2],1})
+         if bot >= top then
+            add(g_out_cache[sind], {x1=i,y1=top,x2=i,y2=bot})
          end
       end
    end
-
-   local x = scr_x(a.x-a.sw*.5)+a.xx
-   local y = scr_y(a.y-a.sh*.5)+a.yy
-   for i=1,#g_spr_out_cache[sind] do
-      rectfill(x+g_spr_out_cache[sind][i][1],y+g_spr_out_cache[sind][i][2],x+g_spr_out_cache[sind][i][3],y+g_spr_out_cache[sind][i][4],g_spr_out_cache[sind][i][5])
-   end
-
-   spr(a.sind, scr_x(a.x-a.sw*.5)+a.xx, scr_y(a.y-a.sh*.5)+a.yy, a.sw, a.sh, a.xf, a.yf)
 end
 
+function spr_out(sind, x, y, sw, sh, xf, yf, col)
+   local ox, x_mult, oy, y_mult = x, 1, y, 1
+   if xf then ox, x_mult = 7+x, 0xffff end
+   if yf then oy, y_mult = 7+y, 0xffff end
+
+   foreach(g_out_cache[sind], function(r)
+      rectfill(
+         ox+x_mult*r.x1,
+         oy+y_mult*r.y1,
+         ox+x_mult*r.x2,
+         oy+y_mult*r.y2,
+         col)
+   end)
+
+   spr(sind, x, y, sw, sh, xf, yf)
+end
 
 -- .2604 (bad outline)
 -- .2537 (working on outline, should get to be smaller).
