6f1fab9a5fdc46621dfdd2451356d44911989a2c
Alan Morgan <alanxoc3@gmail.com>
2021-06-04 21:29:30 -0600

    Created basic template. Will have to work on it more later.

---
+++ TEMPLATE-WIP/src/00-boot.lua
@@ -0,0 +1,11 @@
+-- DEBUG_BEGIN
+-- printh("ZTABLE_STRINGS_FIRST")
+-- printh("ZTABLE_STRINGS_SECOND")
+-- printh("ZTABLE_STRINGS")
+-- printh(g_gunvals_raw)
+-- DEBUG_END
+
+-- DEV_MODE_BEGIN
+-- String data used throughout the cartridge.
+g_gunvals_raw="ZTABLE_STRINGS"
+-- DEV_MODE_END
---
+++ TEMPLATE-WIP/src/04-outlines.lua
@@ -0,0 +1,2 @@
+g_out_cache=ztable[[
+]]
