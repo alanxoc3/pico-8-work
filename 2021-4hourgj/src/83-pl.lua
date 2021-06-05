d7952bff9cd10b120ded1c2f6da0fac4b9b082d2
Alan Morgan <alanxoc3@gmail.com>
2021-06-05 00:08:57 -0600

    meow

--- 2021-4hourgj/src/83-pl.lua
+++ 2021-4hourgj/src/83-pl.lua
@@ -1,4 +1,19 @@
-create_actor([[pl;3;drawable,spr,mov,tcol,col,confined,bounded|
+create_actor([[pl_meow;1;post_drawable,rel|
+    rel_actor:@1;
+    d:@2;
+    u:@3;
+    i:@4;
+]], function(a)
+    zprint("meow", (a.x+a.dx)*8, (a.y+a.dy)*8-2*8, 0, 14, 2)
+end, function(a)
+    if not btn(5) then
+        a:kill()
+    end
+end, function(a)
+    sfx'0'
+end)
+
+create_actor([[pl;2;drawable,spr,mov,tcol,col,confined,bounded|
     x:@1; y:@2;
     sw:2; sh:2;
     xf:no;
@@ -7,8 +22,8 @@ create_actor([[pl;3;drawable,spr,mov,tcol,col,confined,bounded|
     jump_speed:.5;
     sind:6;
     ay:.01;
-    u:@4;
-    tile_hit:@5;
+    u:@3;
+    tile_hit:@4;
 ]], function(a)
     local above_water = a.y < 14
 
@@ -19,8 +34,12 @@ create_actor([[pl;3;drawable,spr,mov,tcol,col,confined,bounded|
         if xbtn() < 0 then a.xf = true end
         if a.touching_ground and btn(4)  then
             a.dy = -a.jump_speed
+            sfx'1'
         end
-        printh(a.dy)
+
+        if btn(5) and (not a.meow or not a.meow.alive) then
+            a.meow = _g.pl_meow(a)
+        end
 
         if not a.touching_ground then
             if a.dy < -.1 then
