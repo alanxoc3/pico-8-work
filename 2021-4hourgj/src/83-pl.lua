027335d60d0c51c40b2a65fd8b0ab84a968dedd7
Alan Morgan <alanxoc3@gmail.com>
2021-06-04 22:29:56 -0600

    Using a cat sprite now.

--- 2021-4hourgj/src/83-pl.lua
+++ 2021-4hourgj/src/83-pl.lua
@@ -4,12 +4,14 @@ create_actor([[pl;3;drawable,spr,mov,tcol,col,confined,x_bounded,y_bounded|
     ix:.90; iy:.98;
     touching_ground:no;
     jump_speed:.5;
-    sind:108;
+    sind:6;
     ay:.01;
     u:@4;
     d:@5;
 ]], function(a)
     a.ax = .02*xbtn()
+    if xbtn() > 0 then a.xf = false end
+    if xbtn() < 0 then a.xf = true end
     if btnp(4) then
         a.dy = -a.jump_speed
     end
