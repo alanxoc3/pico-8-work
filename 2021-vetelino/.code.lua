2d5c019e7d3709c6e66d1b8264d53756f686a012
Alan Morgan <alanxoc3@gmail.com>
2021-07-17 14:55:19 -0600

    Better ice cream truck driving.

--- 2021-vetelino/.code.lua
+++ 2021-vetelino/.code.lua
@@ -183,7 +183,7 @@ end
 end
 return pos,spd
 end
-g_gunvals=split("@1,@2,!plus/@3/1,@5;@1,@2,@3,@4;|0x8000,0x8000,0x7fff,0x7fff,@1|!plus/@1/-2,!plus/@2/-2,!plus/@3/2,!plus/@4/2,13;!plus/@1/-1,!plus/@2/-1,!plus/@3/1,!plus/@4/1,1;|0,0,0,0,0,0,0;1,1,1,0,0,0,0;2,2,2,1,0,0,0;3,3,3,1,0,0,0;4,2,2,2,1,0,0;5,5,1,1,1,0,0;6,13,13,5,5,1,0;7,6,13,13,5,1,0;8,8,2,2,2,0,0;9,4,4,4,5,0,0;10,9,4,4,5,5,0;11,3,3,3,3,0,0;12,12,3,1,1,1,0;13,5,5,1,1,1,0;14,13,4,2,2,1,0;15,13,13,5,5,1,0;|fader_out;3;act,;update,|fade_time:@1;i:@2;e:@3;u:@4;tl_max_time=@1,|fader_in;3;act,;update,||act;0;,;room_init,pause_init,pause_update,pause_end,kill,clean,delete|alive:yes;stun_countdown:0;i:nf;u:nf;update:@1;clean:@2;kill:@3;delete:@4;room_init:nf;create_init:nf;pause_init:nf;pause_update:nf;pause_end:nf;destroyed:nf;get:@5;|ma_able;0;act,;|name:thing;|confined;0;act,;room_end,|room_end:nf;|loopable;0;act,;|tl_loop:yes;|pos;0;act,;|x:0;y:0;|move_pause;0;act,;update,move,vec_update,tick|;|knock;0;col,;|popper;0;col,;|bad;0;knock,;|bounded;0;act,;|check_bounds:nf;|x_bounded;0;bounded,;|check_bounds:@1;|y_bounded;0;bounded,;|timed;0;act,;|t:0;tick:@1;|vec;0;pos,;|dx:0;dy:0;vec_update:@1;|mov;0;vec,;|ix:1;iy:1;ax:0;ay:0;move:@1;stop:@2;calc_next_pos:@3;|dim;0;pos,;|rx:.375;ry:.375;|rel;0;act,;rel_update,|rel_actor:null;rel_x:0;rel_y:0;rel_dx:0;rel_dy:0;flippable:no;rel_update:@1;|drawable_obj;0;pos,;reset_off,|ixx:0;iyy:0;xx:0;yy:0;visible:yes;reset_off:@1;|drawable;0;drawable_obj,;d,|d:nf;|drawable_1;0;drawable_obj,;d,|drawable_2;0;drawable_obj,;d,|pre_drawable;0;drawable_obj,;d,|pre_drawable_1;0;drawable_obj,;d,|pre_drawable_2;0;drawable_obj,;d,|post_drawable;0;drawable_obj,;d,|post_drawable_1;0;drawable_obj,;d,|post_drawable_2;0;drawable_obj,;d,|above_map_post_camera_drawable;0;drawable_obj,;d,|spr_obj;0;vec,drawable_obj,;|sind:0;outline_color:BG_UI;sw:1;sh:1;xf:no;yf:no;draw_spr:@1;draw_out:@2;draw_both:@3;|spr;0;spr_obj,;|d:@1;|knockable;0;mov,;|knockback:@1;|stunnable;0;mov,drawable_obj;|stun_update:@1;|hurtable;0;act,;|health:1;max_health:1;health_visible:yes;hurt:@1;heal:@2;|trig;0;vec,dim;|contains:nf;intersects:nf;not_contains_or_intersects:nf;contains_or_intersects:@1;trigger_update:@1;|anchored;1;vec,dim;|touchable:@1;hit:nf;|col;0;vec,dim;|touchable:yes;hit:nf;move_check:@1;|dx:0;dy:0|x,dx,@1,@2,@3,@4;y,dy,@1,@2,@5,@6;|tcol;0;vec,dim;|tile_solid:yes;tile_hit:nf;coll_tile:@1;|view;4;act,confined;center_view,update_view|x:0;y:0;room_crop:2;tl_loop:yes;w:@1;h:@2;follow_dim:@3;follow_act:@4;update_view:@5;center_view:@6;change_ma:@7;,;|@1,x,w,ixx;@1,y,h,iyy|lane_jumper;0;pos,|x:4;y:4;lane:1;i:@1;switch_lane:@2;|pl;0;drawable,spr,mov,x_bounded|x:4;y:4;ix:.96;iy:.92;sind:39;sh:2;iyy:-4;i:@1;u:@2;|truck;0;drawable,spr,mov|x:14.5;y:4;sind:26;sw:2;sh:3;iyy:-8;ix:.96;iy:.92;i:@1;u:@2;|road_gen;0;act,vec,drawable;update,|x:0;u:@1;d:@2;|i=@3,u=@4,d=@5;|x:2;y:2;w:12;h:16;c:0;|act,clean;act,update;mov,move;vec,vec_update;bounded,check_bounds;|drawable,d;|pre_drawable,d;pre_drawable_1,d;pre_drawable_2,d;|drawable,d;drawable_1,d;drawable_2,d;post_drawable,d;post_drawable_1,d;post_drawable_2,d;|","|")
+g_gunvals=split("@1,@2,!plus/@3/1,@5;@1,@2,@3,@4;|0x8000,0x8000,0x7fff,0x7fff,@1|!plus/@1/-2,!plus/@2/-2,!plus/@3/2,!plus/@4/2,13;!plus/@1/-1,!plus/@2/-1,!plus/@3/1,!plus/@4/1,1;|0,0,0,0,0,0,0;1,1,1,0,0,0,0;2,2,2,1,0,0,0;3,3,3,1,0,0,0;4,2,2,2,1,0,0;5,5,1,1,1,0,0;6,13,13,5,5,1,0;7,6,13,13,5,1,0;8,8,2,2,2,0,0;9,4,4,4,5,0,0;10,9,4,4,5,5,0;11,3,3,3,3,0,0;12,12,3,1,1,1,0;13,5,5,1,1,1,0;14,13,4,2,2,1,0;15,13,13,5,5,1,0;|fader_out;3;act,;update,|fade_time:@1;i:@2;e:@3;u:@4;tl_max_time=@1,|fader_in;3;act,;update,||act;0;,;room_init,pause_init,pause_update,pause_end,kill,clean,delete|alive:yes;stun_countdown:0;i:nf;u:nf;update:@1;clean:@2;kill:@3;delete:@4;room_init:nf;create_init:nf;pause_init:nf;pause_update:nf;pause_end:nf;destroyed:nf;get:@5;|ma_able;0;act,;|name:thing;|confined;0;act,;room_end,|room_end:nf;|loopable;0;act,;|tl_loop:yes;|pos;0;act,;|x:0;y:0;|move_pause;0;act,;update,move,vec_update,tick|;|knock;0;col,;|popper;0;col,;|bad;0;knock,;|bounded;0;act,;|check_bounds:nf;|x_bounded;0;bounded,;|check_bounds:@1;|y_bounded;0;bounded,;|timed;0;act,;|t:0;tick:@1;|vec;0;pos,;|dx:0;dy:0;vec_update:@1;|mov;0;vec,;|ix:1;iy:1;ax:0;ay:0;move:@1;stop:@2;calc_next_pos:@3;|dim;0;pos,;|rx:.375;ry:.375;|rel;0;act,;rel_update,|rel_actor:null;rel_x:0;rel_y:0;rel_dx:0;rel_dy:0;flippable:no;rel_update:@1;|drawable_obj;0;pos,;reset_off,|ixx:0;iyy:0;xx:0;yy:0;visible:yes;reset_off:@1;|drawable;0;drawable_obj,;d,|d:nf;|drawable_1;0;drawable_obj,;d,|drawable_2;0;drawable_obj,;d,|pre_drawable;0;drawable_obj,;d,|pre_drawable_1;0;drawable_obj,;d,|pre_drawable_2;0;drawable_obj,;d,|post_drawable;0;drawable_obj,;d,|post_drawable_1;0;drawable_obj,;d,|post_drawable_2;0;drawable_obj,;d,|above_map_post_camera_drawable;0;drawable_obj,;d,|spr_obj;0;vec,drawable_obj,;|sind:0;outline_color:BG_UI;sw:1;sh:1;xf:no;yf:no;draw_spr:@1;draw_out:@2;draw_both:@3;|spr;0;spr_obj,;|d:@1;|knockable;0;mov,;|knockback:@1;|stunnable;0;mov,drawable_obj;|stun_update:@1;|hurtable;0;act,;|health:1;max_health:1;health_visible:yes;hurt:@1;heal:@2;|trig;0;vec,dim;|contains:nf;intersects:nf;not_contains_or_intersects:nf;contains_or_intersects:@1;trigger_update:@1;|anchored;1;vec,dim;|touchable:@1;hit:nf;|col;0;vec,dim;|touchable:yes;hit:nf;move_check:@1;|dx:0;dy:0|x,dx,@1,@2,@3,@4;y,dy,@1,@2,@5,@6;|tcol;0;vec,dim;|tile_solid:yes;tile_hit:nf;coll_tile:@1;|view;4;act,confined;center_view,update_view|x:0;y:0;room_crop:2;tl_loop:yes;w:@1;h:@2;follow_dim:@3;follow_act:@4;update_view:@5;center_view:@6;change_ma:@7;,;|@1,x,w,ixx;@1,y,h,iyy|lane_jumper;0;pos,|x:4;y:4;lane:1;i:@1;switch_lane:@2;|vehicle;0;drawable,spr,mov,x_bounded|x:4;y:4;ix:.96;iy:.92;sind:39;sh:2;iyy:-4;vehicle_logic:nf;i:@1;u:@2;move_x:@3;move_y:@4;|pl;0;vehicle,|x:4;y:4;sind:39;sh:2;iyy:-4;vehicle_logic:@1;|truck;0;vehicle,|x:12;y:4;sind:26;sw:2;sh:3;iyy:-8;vehicle_logic:@1;horizontal_input:0;|road_gen;0;act,vec,drawable;update,|x:0;u:@1;d:@2;|i=@3,u=@4,d=@5;|x:1;y:2;w:14;h:16;c:0;|act,clean;act,update;mov,move;vec,vec_update;bounded,check_bounds;|drawable,d;|pre_drawable,d;pre_drawable_1,d;pre_drawable_2,d;|drawable,d;drawable_1,d;drawable_2,d;post_drawable,d;post_drawable_1,d;post_drawable_2,d;|","|")
 g_ztable_cache={}
 function nf()end
 function ztable(original_str,...)
@@ -596,7 +596,16 @@ end)
 create_actor([[77|78]],function(a)
 a.lane_jumper=_g.lane_jumper()
 end,function(a)
-a.ax=xbtn()*.005
+a:vehicle_logic()
+amov_y(a,.02,a.lane_jumper.y)
+end,function(a,x)
+a.ax=x*.005
+end,function(a,y)
+a.lane_jumper:switch_lane(y)
+end)
+create_actor([[79|80]],function(a)
+a:move_x(xbtn())
+a:move_y(ybtnp())
 if a.dx>.05 and a.ax>0 then
 a.sind=38
 elseif a.dx<-.05 and a.ax<0 then
@@ -604,23 +613,19 @@ a.sind=37
 else
 a.sind=39
 end
-a.lane_jumper:switch_lane(ybtnp())
-amov_y(a,.02,a.lane_jumper.y)
-end,function(a)
-scr_circ(a.x,a.y,.5,12)
-end)
-create_actor([[79|80]],function(a)
-a.lane_jumper=_g.lane_jumper()
-end,function(a)
-a.ax=0
-amov_y(a,.02,a.lane_jumper.y)
-if flr_rnd(30)==0 then
-a.lane_jumper:switch_lane(flr_rnd(3)-1)
-end
 end,function(a)
 scr_circ(a.x,a.y,.5,12)
 end)
 create_actor([[81|82]],function(a)
+if flr_rnd(15)==0 then
+a.horizontal_input=flr_rnd(3)-1
+end
+a:move_x(a.horizontal_input)
+if flr_rnd(30)==0 then
+a:move_y(flr_rnd(3)-1)
+end
+end)
+create_actor([[83|84]],function(a)
 a.dx=-.14
 end,function(a)
 for y=TOP_LANE_Y,BOT_LANE_Y,2 do
@@ -634,7 +639,7 @@ poke(0x5f5c,15)
 poke(0x5f5d,15)
 function _init()
 music(0,3000)
-g_tl=ztable([[83]],logo_draw,function()sfx"63" end,
+g_tl=ztable([[85]],logo_draw,function()sfx"63" end,
 game_init,game_update,game_draw
 )
 end
@@ -643,16 +648,16 @@ _g.fader_in(.5,nf,nf)
 _g.pl()
 _g.truck()
 _g.road_gen()
-g_cur_room=ztable[[84]]
+g_cur_room=ztable[[86]]
 end
 function game_update(a)
-batch_call_new(acts_loop,[[85]])
+batch_call_new(acts_loop,[[87]])
 end
 function game_draw(a)
 fade(g_card_fade)
 draw_blueprint()
 isorty(g_act_arrs.drawable)
-batch_call_new(acts_loop,[[86]])
+batch_call_new(acts_loop,[[88]])
 end
 function logo_draw(a)
 local logo_opacity=8+cos(a.tl_tim/a.tl_max_time)*4-4
@@ -689,9 +694,9 @@ local y1,y2=ry*8+4,(ry+view.h)*8-5
 camera_to_view(view)
 zclip(x1,y1,x2,y2)
 zcls(g_cur_room.c)
-batch_call_new(acts_loop,[[87]])
+batch_call_new(acts_loop,[[89]])
 isorty(g_act_arrs.drawable)
-batch_call_new(acts_loop,[[88]])
+batch_call_new(acts_loop,[[90]])
 clip()
 camera()
 end
