76983d9052cd2bfad50128baae182247f8aa18db
Alan Morgan <alanxoc3@gmail.com>
2021-09-06 12:46:11 -0600

    very basic enemy

--- 2021-wgj-217/src/enemy.lua
+++ 2021-wgj-217/src/enemy.lua
@@ -1,1 +1,14 @@
-function create_enemy(x, y) end
+function create_enemy(x, y)
+    return {
+        dx=0, dy=0,
+        rx=4, ry=4,
+        x=x, y=y,
+        draw=function(a)
+            rect(a.x-a.rx, a.y-a.ry, a.x+a.rx, a.y+a.ry, 2)
+        end,
+        update=function(a)
+            a.dx = cos(t()/10)/2
+            a.dy = sin(t()/10)/2
+        end
+    }
+end
