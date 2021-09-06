8ffd5703529945ca4024077365d71cf7c3b6e87b
Alan Morgan <alanxoc3@gmail.com>
2021-09-06 14:07:41 -0600

    Got an energy bar now.

--- 2021-wgj-217/src/pl.lua
+++ 2021-wgj-217/src/pl.lua
@@ -1,9 +1,12 @@
+MAX_ENERGY=100
+
 function create_pl(x, y)
     return {
         dx=0, dy=0,
         rx=4, ry=4,
         dir=0, -- 0=l, 1=r, 2=u, 3=d
         pressing=nil, -- nil=nothing, 0=l, 1=r, 2=u, 3=d
+        energy=MAX_ENERGY,
         bullet_cooldown=0,
         x=x, y=y,
         friction=.925,
@@ -12,17 +15,19 @@ function create_pl(x, y)
         end,
         update=function(a)
             a.bullet_cooldown += 1
+            a.energy = min(MAX_ENERGY, a.energy+.25)
             a.dx -= pl_button_logic(a, 0, 1)
             a.dx += pl_button_logic(a, 1, 0)
             a.dy -= pl_button_logic(a, 2, 3)
             a.dy += pl_button_logic(a, 3, 2)
 
-            if btn(4) and a.bullet_cooldown >= 15 then
+            if btn(4) and a.energy - 10 >= 0 and a.bullet_cooldown >= 15 then
                 local dx = get_x_from_dir(a.dir, abs(a.dx)+2)
                 local dy = get_y_from_dir(a.dir, abs(a.dy)+2)
                 add(bullets, create_bullet(a.x, a.y, dx, dy))
                 add(bullets, create_bullet(a.x, a.y, dx+dy*.25, dy+dx*.25))
                 add(bullets, create_bullet(a.x, a.y, dx-dy*.25, dy-dx*.25))
+                a.energy -= 10
                 a.bullet_cooldown = 0
             end
 
@@ -65,4 +70,13 @@ function create_bullet(x, y, dx, dy)
             end
         end
     }
+end
+
+function draw_energy_bar(energy, max_energy)
+    local bar_drawn = energy/max_energy * 119
+    if bar_drawn > 0 then
+
+        rectfill(4, 119, 4 + bar_drawn, 124, 12)
+        rect(4, 124, 4 + bar_drawn, 124, 1)
+    end
 end
