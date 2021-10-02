ceef8db29500c336607963cb3f58e6ef64559021
Alan Morgan <alanxoc3@gmail.com>
2021-10-02 02:43:15 -0600

    Some sort of rolling works now works.

--- 2021-ld49/src/02-pl.lua
+++ 2021-ld49/src/02-pl.lua
@@ -1,8 +1,10 @@
 create_actor([[pl;2;drawable,pos,confined,mov,x_bounded,y_bounded,timer|
     x:@1; y:@2;
     
-    inertia_x:.95;
-    inertia_y:.95;
+    inertia_x:.90;
+    inertia_y:.90;
+
+    dir:0;
 
     timer:nil;
 
@@ -10,15 +12,24 @@ create_actor([[pl;2;drawable,pos,confined,mov,x_bounded,y_bounded,timer|
 ]], function(a)
     scr_circ(a.x, a.y, .5, 4)
 end, function(a)
-    if not a:is_timer_active"cooldown" and not a:is_timer_active"speedup" and btn(4) then
-        a:create_timer("speedup", 30, function() a:create_timer("cooldown", 60) end)
+    -- roll
+    if btn(4) and not a:is_timer_active"cooldown" and not a:is_timer_active"roll" then
+        a:create_timer("roll", 10, function() a:create_timer("cooldown", 20) end)
     end
 
-    if a:is_timer_active"speedup" then
-        a.ax = xbtn()*.05
-        a.ay = ybtn()*.05
+    local is_moving = xbtn() ~= 0 or ybtn() ~= 0
+    if is_moving then
+        a.dir = atan2(xbtn(), ybtn())
+    end
+
+    if a:is_timer_active"roll" then
+        a.ax = cos(a.dir)*.05
+        a.ay = sin(a.dir)*.05
+    elseif is_moving then
+        a.ax = cos(a.dir)*.01
+        a.ay = sin(a.dir)*.01
     else
-        a.ax = xbtn()*.01
-        a.ay = ybtn()*.01
+        a.ax = 0
+        a.ay = 0
     end
 end)
