efadc8d028d2779fc16b4eb83f930037478cbd5e
Alan Morgan <alanxoc3@gmail.com>
2021-10-02 11:56:35 -0600

    Awesome jump.

--- 2021-ld49/src/02-pl.lua
+++ 2021-ld49/src/02-pl.lua
@@ -1,13 +1,11 @@
-create_actor([[fist;3;drawable,col,confined,rel|
+create_actor([[fist;3;col,confined,rel|
     rel_actor:@1; x:@2; y:@3; i:@4; d:@5;
-    touchable:no; rx:.25; ry:.25;
+    touchable:no; rx:.25; ry:.5;
 
-    tl_max_time=.125,;
+    tl_max_time=.33,;
 ]], function(a)
-    a.rel_dx = cos(a.rel_actor.dir)*.2
-    a.rel_dy = sin(a.rel_actor.dir)*.2
-end, function(a)
-    scr_circ(a.x, a.y, .3, 8)
+    a.rel_dx = cos(a.rel_actor.dir)*.05
+    a.rel_dy = sin(a.rel_actor.dir)*.05
 end)
 
 create_actor([[pl;2;drawable,pos,confined,mov,x_bounded,y_bounded,col,spr_obj,knockbackable,hurtable|
@@ -43,20 +41,12 @@ end, function(a)
             a:create_timer("roll",  12, function() a.dx /= 3 a.dy /= 3 a:create_timer("cooldown", 20) end)
             
         elseif btn(5) then
-            a:create_timer("punch", 10, function() a:create_timer("cooldown", 10) end)
+            a:create_timer("punch", 20, function() a:create_timer("cooldown", 10) end)
             _g.fist(a, a.x, a.y)
         end
     end
 
     local is_moving = xbtn() ~= 0 or ybtn() ~= 0
-    if is_moving then
-        a.dir = atan2(xbtn(), ybtn())
-    end
-
-    if xbtn() ~= 0 then
-        a.is_facing_left = xbtn() < 0
-    end
-
     if a:any_timer_active"knockback" then
         a:apply_knockback()
     elseif a:any_timer_active"roll" then
@@ -66,9 +56,17 @@ end, function(a)
         a.ax = cos(a.dir)*.01
         a.ay = sin(a.dir)*.01
     elseif is_moving then -- and not a:any_timer_active"punch" then
-        a.ax = cos(a.dir)*.01
-        a.ay = sin(a.dir)*.01
+        if xbtn() ~= 0 then
+            a.is_facing_left = xbtn() < 0
+        end
+
+        a.dir = atan2(xbtn() == 0 and (a.is_facing_left and -1 or 1) or xbtn(), ybtn())
+
+        local dir = atan2(xbtn(), ybtn())
+        a.ax = cos(dir)*.01
+        a.ay = sin(dir)*.01
     else
+        a.dir = atan2(a.is_facing_left and -1 or 1, 0)
         a.ax = 0
         a.ay = 0
     end
