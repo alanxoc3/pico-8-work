c7538528f0b9c94ce16688bda9eee7ebc3557bb6
Alan Morgan <alanxoc3@gmail.com>
2021-10-02 15:38:48 -0600

    Nurse walk better I guess.

--- 2021-ld49/.code.lua
+++ 2021-ld49/.code.lua
@@ -636,8 +636,11 @@ end
 end,function(a)
 a.sind=66
 if abs(a.dx)>.005 or abs(a.dy)>.005 then
-if a.tl_tim%.5<.25 then
-a.sind=67
+local loop=a.tl_tim%.5/.5
+if loop<.25 then a.sind=66
+elseif loop<.5 then a.sind=67
+elseif loop<.75 then a.sind=66
+else a.sind=68
 end
 end
 scr_spr(a)
