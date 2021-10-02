c2c9ca0a406a1b89027bbe00b5b77c7bc08068ae
Alan Morgan <alanxoc3@gmail.com>
2021-10-02 02:31:48 -0600

    Created a timer api.
    I think this will help me manage state.

--- 2021-ld49/src/02-pl.lua
+++ 2021-ld49/src/02-pl.lua
@@ -1,13 +1,24 @@
-create_actor([[pl;2;drawable,pos,confined,mov,x_bounded,y_bounded|
+create_actor([[pl;2;drawable,pos,confined,mov,x_bounded,y_bounded,timer|
     x:@1; y:@2;
     
     inertia_x:.95;
     inertia_y:.95;
 
+    timer:nil;
+
     d:@3; u:@4;
 ]], function(a)
     scr_circ(a.x, a.y, .5, 4)
 end, function(a)
-    a.ax = xbtn()*.01
-    a.ay = ybtn()*.01
+    if not a:is_timer_active"cooldown" and not a:is_timer_active"speedup" and btn(4) then
+        a:create_timer("speedup", 30, function() a:create_timer("cooldown", 60) end)
+    end
+
+    if a:is_timer_active"speedup" then
+        a.ax = xbtn()*.05
+        a.ay = ybtn()*.05
+    else
+        a.ax = xbtn()*.01
+        a.ay = ybtn()*.01
+    end
 end)
