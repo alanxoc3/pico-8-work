82c76eb1daf44a04eb560ff987da01ae875898e7
Alan Morgan <alanxoc3@gmail.com>
2021-10-02 15:49:05 -0600

    Jump moves much less.

--- 2021-ld49/.code.lua
+++ 2021-ld49/.code.lua
@@ -731,8 +731,8 @@ elseif a:any_timer_active"roll"then
 a.ax=cos(a.dir)*.03
 a.ay=sin(a.dir)*.03
 elseif a:any_timer_active"punch"then
-a.ax=cos(a.dir)*.015
-a.ay=sin(a.dir)*.015
+a.ax=cos(a.dir)*.005
+a.ay=sin(a.dir)*.005
 elseif is_moving then
 if xbtn()~=0 then
 a.is_facing_left=xbtn()<0
