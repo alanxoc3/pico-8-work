00d203c823530001731f41f9e59a7e0372a97023
Alan Morgan <alanxoc3@gmail.com>
2021-10-03 23:50:24 -0600

    De-cluttered game file.

--- 2021-ld49/.code.lua
+++ 2021-ld49/.code.lua
@@ -1048,33 +1048,44 @@ end)
 create_actor([[111|107]],function(a)
 control_player(a,xbtn(),ybtn(),btn(4),btn(5),false)
 end)
-function reset_the_dungeon()
-batch_call_new(acts_loop,[[112]])
-for body in all(_g.all_deadbody_templates)do
-_g.deadbody_nobleed(unpack(body))
-end
-for i,enemy_template in pairs(_g.all_enemy_templates)do
-if enemy_template.alive then
-enemy_template.func(enemy_template.x,enemy_template.y,i)
-end
-end
-_g.fader_in(.5,nf,nf)
-g_room=ztable[[113]]
-if _g.c_is_patient then
-g_pl=_g.pl_patient_control_fight(7,7)
-else
-g_pl=_g.pl_monster_control(7,7)
-end
-g_view=_g.view(15.25,11.5,3,g_pl)
+function create_ui_hearts()
 _g.heart_particle_spawner(6.5,1,3)
 _g.heart_particle_spawner(9.5,1,2)
 _g.heart_particle_spawner(8,1,1)
-_g.genocide_tip(8,13)
+end
+function create_ui_powerups()
 _g.powerup_particle_spawner(3,1,0,_g.c_color_normal,72)
 _g.powerup_particle_spawner(4,15,1,_g.c_color_insane_1,74)
 _g.powerup_particle_spawner(8,15,2,_g.c_color_insane_2,76)
 _g.powerup_particle_spawner(12,15,3,_g.c_color_insane_3,78)
 _g.powerup_particle_spawner(13,1,4,_g.c_color_angry,104)
+end
+function create_all_enemies()
+for i,enemy_template in pairs(_g.all_enemy_templates)do
+if enemy_template.alive then
+enemy_template.func(enemy_template.x,enemy_template.y,i)
+end
+end
+end
+function create_all_deadbodies()
+for body in all(_g.all_deadbody_templates)do
+_g.deadbody_nobleed(unpack(body))
+end
+end
+function cleanup_confined()
+batch_call_new(acts_loop,[[112]])
+end
+function reset_the_dungeon()
+cleanup_confined()
+_g.fader_in(.5,nf,nf)
+g_pl=_g.pl_monster_control(7,7)
+g_view=_g.view(15.25,11.5,3,g_pl)
+g_room=ztable[[113]]
+create_all_deadbodies()
+create_all_enemies()
+create_ui_hearts()
+create_ui_powerups()
+_g.genocide_tip(8,13)
 end
 function game_init()
 _g.all_deadbody_templates={}
