fd2038a18851d543bcfbc1954f1b5d667a5fa2a5
Daniel Avery <danielaveryj@gmail.com>
2021-10-04 15:21:20 -0600

    Fix dungeon spawn position and tweak end portal position.

--- 2021-ld49/src/97-rooms.lua
+++ 2021-ld49/src/97-rooms.lua
@@ -103,8 +103,7 @@ function reset_the_dungeon()
     cleanup_confined()
 
     _g.fader_in(.5, nf, nf)
-    -- g_pl = _g.pl_monster_control(17, 4)
-    g_pl = _g.pl_monster_control(94, 12)
+    g_pl = _g.pl_monster_control(18, 3)
     g_view = _g.view(15.25, 11.5, 3, g_pl)
     g_room = ztable[[ name:dungeon; x:0; y:0; w:108; h:32; ]]
 
@@ -124,7 +123,7 @@ function reset_the_hospital()
     cleanup_confined()
 
     _g.fader_in(.5, nf, nf)
-    _g.portal(17, 4)
+    _g.portal(18, 3)
     g_pl = _g.pl_patient_control_passive(94, 13)
     g_view = _g.view(15.25, 11.5, 3, g_pl)
     g_room = ztable[[ name:hospital; x:0; y:0; w:108; h:32; ]]
