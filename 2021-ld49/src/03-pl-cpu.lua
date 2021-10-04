e4f474de677ee7fa16a962dc4a58c79097833d8e
Alan Morgan <alanxoc3@gmail.com>
2021-10-04 16:34:54 -0600

    Improvement to hits.

--- 2021-ld49/src/03-pl-cpu.lua
+++ 2021-ld49/src/03-pl-cpu.lua
@@ -2,9 +2,9 @@ create_actor([[pl_monster_cpu;3;pl_monster,bad_character|
     touchable:no; hurt_end:nf;
     x:@1; y:@2; enemy_id:@3; i:@4; u:@5; destroyed:@6;;
 ]], function(a)
-    a:set_insanity(3)
+    a:set_strength(3)
 end, function(a)
-    control_player(a, flr_rnd(3)-1, flr_rnd(3)-1, flr_rnd(a.insane_level+5) == 0, flr_rnd(4-a.insane_level) == 0, _g.bad_fist, a.insane_level)
+    control_player(a, flr_rnd(3)-1, flr_rnd(3)-1, flr_rnd(a.strength+5) == 0, flr_rnd(4-a.strength) == 0, _g.bad_fist, a.strength)
 end, function(a)
     a:create_dead_body()
     _g.portal(118,28)
