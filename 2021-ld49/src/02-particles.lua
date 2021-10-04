c671fda4bcf7023f6c5376def4b770c81bb3932e
Alan Morgan <alanxoc3@gmail.com>
2021-10-03 23:08:55 -0600

    Genocide tip.

--- 2021-ld49/src/02-particles.lua
+++ 2021-ld49/src/02-particles.lua
@@ -34,7 +34,6 @@ create_actor[[slow_medium_ui_particle;3;above_map_drawable,particle_parent|
     tl_max_time=.25,;
 ]]
 
-
 create_parent([[particle_spawner;5;dim,confined|
     color: 8;
     rx:@1; ry:@2; rate:@3; chance:@4; create_func:@5; update_particles:@6;
@@ -47,28 +46,5 @@ create_parent([[particle_spawner;5;dim,confined|
                 a.color
             )
         end
-    end
-end)
-
-create_actor([[heart_particle_spawner;3;particle_spawner/.125/.125/1/3/%slow_small_ui_particle,above_map_drawable_1|
-    x:@1; y:@2; heart_number:@3; u:@4; d:@5; color: 8; 
-]], function(a)
-    a:update_particles()
-    if g_pl.health < a.heart_number then
-        a:kill()
-    end
-end, function(a)
-    zspr(16, a.x*8, a.y*8+1)
-end)
-
-create_actor([[powerup_particle_spawner;5;particle_spawner/2/.125/1/1/%slow_medium_ui_particle,above_map_drawable_1|
-    x:@1; y:@2; powerup_level:@3; color:@4; sind:@5; u:@6; d:@7
-]], function(a)
-    if g_pl.alive and g_pl.insane_level == a.powerup_level then
-        a:update_particles()
-    end
-end, function(a)
-    if g_pl.alive and g_pl.insane_level == a.powerup_level then
-        zspr(a.sind, a.x*8, a.y*8, 2, 2, g_pl.is_facing_left, false)
     end
 end)
