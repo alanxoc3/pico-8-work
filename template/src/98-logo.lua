ef7539955e1b736fa3ebc7c0305aa0d5b37c9a1f
Alan Morgan <alanxoc3@gmail.com>
2022-03-21 00:00:48 -0600

    WIP - logo in template.

---
+++ template/src/98-logo.lua
@@ -0,0 +1,53 @@
+-- x=64, y=64, i=@2, u=nf, d=@1, tl_max_time=2.5; -- logo
+
+function zspr(sind, x, y, sw, sh, ...)
+   sw, sh = sw or 1, sh or 1
+   spr(sind, x-sw*4, y-sh*4, sw, sh, ...)
+end
+
+function flr_rnd(x)
+   return flr(rnd(x))
+end
+
+function rnd_one(val)
+    return (flr_rnd'3'-1)*(val or 1)
+end
+
+g_fade_table = ztable[[
+0;,0,0,0,0,0,0,0,0;
+1;,1,1,1,1,0,0,0,0;
+2;,2,2,2,2,1,0,0,0;
+3;,3,3,3,3,1,0,0,0;
+4;,4,4,2,2,2,1,0,0;
+5;,5,5,5,1,1,1,0,0;
+6;,6,6,13,13,5,5,1,0;
+7;,7,7,6,13,13,5,1,0;
+8;,8,8,8,2,2,2,0,0;
+9;,9,9,4,4,4,5,0,0;
+10;,10,10,9,4,4,5,5,0;
+11;,11,11,3,3,3,3,0,0;
+12;,12,12,12,3,1,1,1,0;
+13;,13,13,5,5,1,1,1,0;
+14;,14,14,13,4,2,2,1,0;
+15;,15,15,13,13,5,5,1,0;
+]]
+
+function fade(i)
+    for c=0,15 do
+        pal(c,g_fade_table[c][min(flr(i+1), 7)])
+    end
+end
+
+|logo_init| function() sfx'63' end $$
+
+|logo_draw| function(a)
+   -- stateful: load(state)
+   -- timed_state: state_duration, state_tick, state_next=nil (end)
+   local logo_opacity = 8+cos(a.state_tick/a.state_duration)*4-4
+
+   fade(logo_opacity)
+   camera(logo_opacity > 1 and rnd_one())
+   zspr(108, a.x, a.y, 4, 2)
+   fade'0'
+   camera()
+end $$
--- template/src/99-main.lua
+++ template/src/99-main.lua
@@ -34,18 +34,23 @@ function xbtn() return btn_helper(btn, 0, 1) end
 function ybtn() return btn_helper(btn, 2, 3) end
 
 function _init()
-    g_tl = ztable[[u,%game_update, d,%game_draw]]
+    g_tl = ztable[[u,%game_update, d,%logo_draw]]
     ball = _g.ball(50, 50)
 end
+
 
 |ball_draw| function(a)
     circ(a.x, a.y, a.radius, a.color)
 end $$
 
-|game_draw| function()
-    cls()
-    print("hello world", 64, 64, 8)
-    ball:draw()
+|logo_draw| function(a)
+   local logo_opacity = 8+cos(a.tl_tim/a.tl_max_time)*4-4
+
+   fade(logo_opacity)
+   camera(logo_opacity > 1 and rnd_one())
+   zspr(108, a.x, a.y, 4, 2)
+   fade'0'
+   camera()
 end $$
 
 |game_update| function()
