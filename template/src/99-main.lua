93ad4f660dc1d5295b51a2bd64299d3aedeea425
Alan Morgan <alanxoc3@gmail.com>
2022-03-23 17:25:10 -0600

    Template working & much more final. No more lib dir. Also changed logo tune :).

--- template/src/99-main.lua
+++ template/src/99-main.lua
@@ -1,38 +1,35 @@
--- overview of basic game loop:
--- * create things
--- * tick (start at 0)
--- * update
--- * destroy / delete
--- * draw
-
-_g = G_TABLE_INITIALIZATION
+-- TEMPLATE TOKEN COUNT: 910
 
 zobj[[,game_state,actor|
+    actor, null; -- remove game_state from the actor list
     curr,logo;
     logo; init,%logo_init, update,%logo_update, draw,%logo_draw, duration,2.5, next,game;
-    game; init,nop,        update,%game_update, draw,%game_draw;
+    game; init,%game_init, update,%game_update, draw,%game_draw;
 ]]
-
-|game_update| function()
-    -- loop_zobjs('actor', 'state')
-end $$
-
-|game_draw| function()
-    loop_zobjs('drawable', 'draw')
-end $$
 
 function _init()
     g_tl = _g.game_state()
 end
 
 function _update60()
-    register_zobjs()
-    loop_zobjs('timer', 'tick')
-    loop_zobjs('actor', 'state')
-    loop_zobjs('actor', 'clean')
+    register_zobjs()                  -- register all zobs from previous game loop iteration
+    loop_zobjs('timer',      'tick')  -- update the timers
+    loop_zobjs('game_state', 'state') -- game state controls the different overall states in the game
+    loop_zobjs('actor',      'clean') -- delete actors and call their "destroy" function at the end of the loop
 end
 
 function _draw()
     cls()
     loop_zobjs('game_state', 'draw')
 end
+
+-- BASIC EXAMPLE FOR SIMPLE GAME BELOW:
+zobj[[,test_obj,actor,drawable|x,@,y,@,color,7,init,%test_init,update,%test_update,draw,%test_draw;]]
+|test_init|   function(a) a.color += 1                   end $$
+|test_update| function(a) a.x += xbtn() a.y += ybtn()    end $$
+|test_draw|   function(a) circfill(a.x, a.y, 2, a.color) end $$
+
+|game_init|   function() _g.test_obj(64, 64)             end $$
+|game_update| function() loop_zobjs('actor', 'state')    end $$
+|game_draw|   function() rect(0, 0, 127, 127, 8)
+                         loop_zobjs('drawable', 'draw')  end $$
