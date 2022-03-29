f6bd795d0127dc00ed95d9350794bd0f2b1dcfdc
Daniel Avery <danielaveryj@gmail.com>
2022-03-28 22:02:49 -0600

    Go back to Alan's way of doing timers, it's easier to implement what we need and scales.

--- template/src/03-timer.lua
+++ template/src/03-timer.lua
@@ -1,9 +1,8 @@
 -- < 152 tokens
 
--- A timer object is able to keep track of multiple timers. a callback can be
+-- A timer object is able to keep track of multiple timers. A callback can be
 -- optionally called when each timer completes. Due to pico-8 constraints, there
--- is a default maximum of a little over 9 hours. At that point, the global time()
--- will overflow to a negative number, and timers will cease to work as expected.
+-- is a default maximum duration of a little over 9 hours. 
 
 zclass[[timer|
     timers;             ,;
@@ -16,8 +15,7 @@ zclass[[timer|
 
 |timer_set_timer| function(a, timer_name, duration, callback)
     -- Note: Should use 0 < duration < 32767 (measured in sec; about 9.1 hrs).
-    -- Since we rely on the global time(), expect the game to come to a screeching conclusion after 9.1 hrs.
-    a.timers[timer_name] = { done=false, start=time(), duration=duration or 32767, callback=callback or function() end }
+    a.timers[timer_name] = { elapsed=0, duration=duration or 32767, callback=callback or function() end }
 end $$
 
 |timer_delete_timer| function(a, timer_name)
@@ -26,20 +24,23 @@ end $$
 
 |timer_get_elapsed| function(a, timer_name)
     local timer = a.timers[timer_name]
-    return timer and (time()-timer.start)
+    return timer and timer.elapsed
 end $$
 
 |timer_get_elapsed_percent| function(a, timer_name)
     local timer = a.timers[timer_name]
-    return timer and (time()-timer.start)/timer.duration
+    return timer and timer.elapsed/timer.duration
 end $$
 
+-- This is expected to be called once per frame (at 60 FPS)!
 |timer_tick| function(a)
     local finished_timers = {}
     for name, timer in pairs(a.timers) do
-        if not timer.done and time()-timer.duration >= timer.start then
-            timer.done = true
-            add(finished_timers, timer)
+        if timer.elapsed < timer.duration then
+            timer.elapsed = timer.elapsed + 1/60
+            if timer.elapsed >= timer.duration then
+                add(finished_timers, timer)
+            end
         end
     end
 
