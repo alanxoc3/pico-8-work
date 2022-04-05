37ccf1a5d55388a3530812424a232225b99a9105
Daniel Avery <danielaveryj@gmail.com>
2022-04-04 18:50:09 -0600

    Spawn chasers during level.

--- 2022-ld50/src/97-level-mouse.lua
+++ 2022-ld50/src/97-level-mouse.lua
@@ -1,10 +1,26 @@
 
+zclass[[mouse_level_state,actor|
+    planet,@,
+    chasers_spawned,0;
+    start;duration,10,next,spawn;
+    spawn;init,%spawn_init,duration,8,next,spawn;
+    done_spawning;,;
+]]
+
+|spawn_init| function(a)
+    printh('here')
+    local ang = rnd()
+    local x,y = cos(ang)*20,sin(ang)*20
+    _g.chaser(x, y, a.planet)
+    a.chasers_spawned += 1
+    if a.chasers_spawned == 9 then a.next = 'done_spawning' end
+end $$
 
 |level_mouse_init| function()
     music(32,1000,7)
     clean_all_entities()
 
-    g_pl = _g.pl(0, 20) -- add player
+    g_pl = _g.pl(0, 0) -- add player
     g_view = _g.view(g_pl)
     local star_view = _g.star_view(g_pl) -- this view is just for stars, so they don't lose their position when the screen wraps
     for i=1,50 do
@@ -23,7 +39,8 @@
 
     -- title
 
-    _g.planet(0,-22,10,_g.MOUSE)
+    local planet = _g.planet(0,-22,10,_g.MOUSE)
+    _g.mouse_level_state(planet)
     _g.chaser(0, 2, planet)
     _g.black_hole(0, 22)
 end $$
@@ -35,17 +52,12 @@ end $$
 
     loop_zobjs('missile', 'collide', g_zclass_entities['teammate'])
     loop_zobjs('teammate', 'collide', g_zclass_entities['teammate'])
-    -- loop_zobjs('chaser', 'collide', g_zclass_entities['pl'])
-    -- loop_zobjs('chaser', 'collide', g_zclass_entities['missile'])
-    -- loop_zobjs('chaser', 'collide', g_zclass_entities['view'])
-    -- loop_zobjs('black_hole', 'collide', g_zclass_entities['pl'])
-    -- loop_zobjs('black_hole', 'collide', g_zclass_entities['view'])
 
     loop_zobjs('alert_radar', 'register', g_zclass_entities['planet'])
     loop_zobjs('alert_radar', 'register', g_zclass_entities['view'])
     loop_zobjs('focus_point', 'collide', g_zclass_entities['view'])
-    --loop_zobjs('alert_radar', 'register', g_zclass_entities['chaser'])
-    --loop_zobjs('alert_radar', 'register', g_zclass_entities['black_hole'])
+    loop_zobjs('alert_radar', 'register', g_zclass_entities['chaser'])
+    loop_zobjs('alert_radar', 'register', g_zclass_entities['black_hole'])
     loop_zobjs('black_hole', 'tug', g_zclass_entities['teammate']) -- affects dx & dy of all 'teammate' objects
 
     loop_zobjs('collision_circ', 'follow_anchoring')
