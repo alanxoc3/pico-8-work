4226a454c62bf9d4572b4a6948aa02cde5792e01
Alan Morgan <alanxoc3@gmail.com>
2022-04-23 13:35:08 -0600

    Moved tweetcarts to archive.

--- work/faces.lua
+++
@@ -1,5 +0,0 @@
--- "Faces".
-c=cos s=sin r=rnd p=pset d=24function e(a)p(a+q*c(y)*s(x)*d+w*c(x)*s(y)*d,48+q*s(y)*c(y)*c(x)*d+w*s(x)*d,7)circfill(a,48,4,0)end
-w=r()::_::x=r()y=r()if t()%1.5<.75then
-p(x*128,y*128,0)w+=.7w%=1else
-e(32)e(96)p(64+s(x)*c(q+y)*d,80+s(y%.5+.5)*d,8)end q=(w+.5)%1goto _
