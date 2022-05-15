ea3716caf959a2cf50b4381779dd107f63573ae5
Alan Morgan <alanxoc3@gmail.com>
2022-05-15 08:30:17 -0600

    Removed 2020-zeldo-game dir as that doesn't compile now.

--- 2020-zeldo-game/src/02-att.lua
+++
@@ -1,8 +0,0 @@
--- If the game is paused, only run paused functions.
-function acts_loop(id, func_name, ...)
-   for a in all(g_act_arrs[id]) do
-      if not is_game_paused() or is_game_paused() and a.pause[func_name] then
-         call_not_nil(a, func_name, a, ...)
-      end
-   end
-end
