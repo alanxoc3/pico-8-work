b04477ee0f68945a316cd335480b75efdc8fa3ec
Alan Morgan <alan@xoc3.io>
2023-01-04 13:54:46 -0700

    Working on compressing things.

---
+++ 2023-pokemon-battle-sim/compile.lua
@@ -0,0 +1,196 @@
+-- get pixels, but not limited to the graphics data section, can go beyond.
+function vget(offset, x, y)
+    x = min(max(0, x), 127)\1
+    y = y\1
+
+    local val = peek(y*64+offset+x\2)
+    if x%2 == 1 then return (val & 0xf0) >>> 4
+    else             return (val & 0x0f) end
+end
+
+-- px9 compression
+function px9_comp(x0,y0,w,h,dest,vget)
+    local dest0=dest
+    local bit=1
+    local byte=0
+
+    local function vlist_val(l, val)
+        -- find position and move
+        -- to head of the list
+
+--[ 2-3x faster than block below
+        local v,i=l[1],1
+        while v!=val do
+            i+=1
+            v,l[i]=l[i],v
+        end
+        l[1]=val
+        return i
+--]]
+
+--[[ 8 tokens smaller than above
+        for i,v in ipairs(l) do
+            if v==val then
+                add(l,deli(l,i),1)
+                return i
+            end
+        end
+--]]
+    end
+
+    local cache,cache_bits=0,0
+    function putbit(bval)
+     cache=cache<<1|bval
+     cache_bits+=1
+        if cache_bits==8 then
+            poke(dest,cache)
+            dest+=1
+            cache,cache_bits=0,0
+        end
+    end
+
+    function putval(val, bits)
+        for i=bits-1,0,-1 do
+            putbit(val>>i&1)
+        end
+    end
+
+    function putnum(val)
+        local bits = 0
+        repeat
+            bits += 1
+            local mx=(1<<bits)-1
+            local vv=min(val,mx)
+            putval(vv,bits)
+            val -= vv
+        until vv<mx
+    end
+
+
+    -- first_used
+
+    local el={}
+    local found={}
+    local highest=0
+    for y=y0,y0+h-1 do
+        for x=x0,x0+w-1 do
+            c=vget(x,y)
+            if not found[c] then
+                found[c]=true
+                add(el,c)
+                highest=max(highest,c)
+            end
+        end
+    end
+
+    -- header
+
+    local bits=1
+    while highest >= 1<<bits do
+        bits+=1
+    end
+
+    putnum(w-1)
+    putnum(h-1)
+    putnum(bits-1)
+    putnum(#el-1)
+    for i=1,#el do
+        putval(el[i],bits)
+    end
+
+
+    -- data
+
+    local pr={} -- predictions
+
+    local dat={}
+
+    for y=y0,y0+h-1 do
+        for x=x0,x0+w-1 do
+            local v=vget(x,y)
+
+            local a=y>y0 and vget(x,y-1) or 0
+
+            -- create vlist if needed
+            local l=pr[a] or {unpack(el)}
+            pr[a]=l
+
+            -- add to vlist
+            add(dat,vlist_val(l,v))
+           
+            -- and to running list
+            vlist_val(el, v)
+        end
+    end
+
+    -- write
+    -- store bit-0 as runtime len
+    -- start of each run
+
+    local nopredict
+    local pos=1
+
+    while pos <= #dat do
+        -- count length
+        local pos0=pos
+
+        if nopredict then
+            while dat[pos]!=1 and pos<=#dat do
+                pos+=1
+            end
+        else
+            while dat[pos]==1 and pos<=#dat do
+                pos+=1
+            end
+        end
+
+        local splen = pos-pos0
+        putnum(splen-1)
+
+        if nopredict then
+            -- values will all be >= 2
+            while pos0 < pos do
+                putnum(dat[pos0]-2)
+                pos0+=1
+            end
+        end
+
+        nopredict=not nopredict
+    end
+
+    if cache_bits>0 then
+        -- flush
+        poke(dest,cache<<8-cache_bits)
+        dest+=1
+    end
+
+    return dest --dest0
+end
+
+
+function log(logstr)
+    printh(logstr)
+    print(logstr)
+    flip()
+end
+
+
+ENCODE_OFFSET=8
+function encode_cart(cartname)
+    reload(0x8000, 0x0000, 0x2000, cartname)
+    ENCODE_OFFSET += px9_comp(0,0,128,128,ENCODE_OFFSET,function(...) return vget(0x8000, ...) end)
+    log("pos: "..ENCODE_OFFSET)
+    return ENCODE_OFFSET
+end
+
+cls()
+memset(0x0000, 0, 0x3100)
+local write_len = 0
+
+poke2(0x0000, ENCODE_OFFSET) encode_cart("001-064.p8")
+poke2(0x0002, ENCODE_OFFSET) encode_cart("065-128.p8")
+poke2(0x0004, ENCODE_OFFSET) encode_cart("129-151.p8")
+poke2(0x0006, ENCODE_OFFSET) encode_cart("pokedex.p8")
+
+cstore(0x0000, 0x0000, 0x3100, "game.p8")
+log("done!")
---
+++ 2023-pokemon-battle-sim/game.lua
@@ -0,0 +1,150 @@
+function extract_sheet(index)
+    memset(0x8000+index*0x2000, 0, 0x2000)
+    px9_decomp(0, 0, peek2(index*2),
+        function(...) return vget(0x8000+index*0x2000, ...) end,
+        function(...) return vset(0x8000+index*0x2000, ...) end)
+end
+
+function _init()
+    extract_sheet(0)
+    extract_sheet(1)
+    extract_sheet(2)
+
+    memcpy(0x0000, 0xe000, 0x2000)
+end
+
+function _draw()
+    cls()
+    memcpy(0x6000, 0x0000, 0x2000)
+    -- spr(0,0,0,16,16)
+end
+
+function vget(offset, x, y)
+    x = min(max(0, x), 127)\1
+    y = y\1
+
+    local val = peek(y*64+offset+x\2)
+    if x%2 == 1 then return (val & 0xf0) >>> 4
+    else             return (val & 0x0f) end
+end
+
+function vset(offset, x, y, val)
+    x = min(max(0, x), 127)\1
+    y = y\1
+
+    local mem_coord = y*64+offset+x\2
+    local oldval = peek(mem_coord)
+
+    if x%2 == 1 then poke(mem_coord, (oldval & 0x0f) | (((val or 0) & 0x0f) << 4))
+                else poke(mem_coord, (oldval & 0xf0) | (((val or 0) & 0x0f) << 0))
+    end
+end
+
+function px9_decomp(x0,y0,src,vget,vset)
+    local function vlist_val(l, val)
+        -- find position and move
+        -- to head of the list
+
+--[ 2-3x faster than block below
+        local v,i=l[1],1
+        while v!=val do
+            i+=1
+            v,l[i]=l[i],v
+        end
+        l[1]=val
+--]]
+
+--[[ 7 tokens smaller than above
+        for i,v in ipairs(l) do
+            if v==val then
+                add(l,deli(l,i),1)
+                return
+            end
+        end
+--]]
+    end
+
+    -- bit cache is between 8 and
+    -- 15 bits long with the next
+    -- bits in these positions:
+    --   0b0000.12345678...
+    -- (1 is the next bit in the
+    --   stream, 2 is the next bit
+    --   after that, etc.
+    --  0 is a literal zero)
+    local cache,cache_bits=0,0
+    function getval(bits)
+        if cache_bits<8 then
+            -- cache next 8 bits
+            cache_bits+=8
+            cache+=@src>>cache_bits
+            src+=1
+        end
+
+        -- shift requested bits up
+        -- into the integer slots
+        cache<<=bits
+        local val=cache&0xffff
+        -- remove the integer bits
+        cache^^=val
+        cache_bits-=bits
+        return val
+    end
+
+    -- get number plus n
+    function gnp(n)
+        local bits=0
+        repeat
+            bits+=1
+            local vv=getval(bits)
+            n+=vv
+        until vv<(1<<bits)-1
+        return n
+    end
+
+    -- header
+
+    local
+        w,h_1,      -- w,h-1
+        eb,el,pr,
+        x,y,
+        splen,
+        predict
+        =
+        gnp"1",gnp"0",
+        gnp"1",{},{},
+        0,0,
+        0
+        --,nil
+
+    for i=1,gnp"1" do
+        add(el,getval(eb))
+    end
+    for y=y0,y0+h_1 do
+        for x=x0,x0+w-1 do
+            splen-=1
+
+            if(splen<1) then
+                splen,predict=gnp"1",not predict
+            end
+
+            local a=y>y0 and vget(x,y-1) or 0
+
+            -- create vlist if needed
+            local l=pr[a] or {unpack(el)}
+            pr[a]=l
+
+            -- grab index from stream
+            -- iff predicted, always 1
+
+            local v=l[predict and 1 or gnp"2"]
+
+            -- update predictions
+            vlist_val(l, v)
+            vlist_val(el, v)
+
+            -- set
+            vset(x,y,v)
+        end
+    end
+end
