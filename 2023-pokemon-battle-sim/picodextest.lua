246c4f6e62cc2660e544a0c26c1f410c258e0452
Alan Morgan <alan@xoc3.io>
2023-01-05 16:29:44 -0700

    More progress. Got screens working.

--- 2023-pokemon-battle-sim/picodextest.lua
+++ 2023-pokemon-battle-sim/picodextest.lua
@@ -5,43 +5,65 @@ function zprint(str, x, y, color, align)
     print(str, x, y, color)
 end
 
-function _draw()
-    -- clear and border
-    cls() -- rect(0,0,127,127,3)
+-- pixels are 0 to 37 (38x38 screen)
+function draw_main_screen()
+    -- local fill_pattern = t()\1 % 2 == 0 and 0b1010010110100101 or 0b0101101001011010
+    local fill_pattern = t()*2\1 % 2 == 0 and 0b1111000011110000 or 0b0000111100001111
+    fillp(fill_pattern)
+    rectfill(0,0,37,37,0x5d)
+    fillp()
+    sspr(0,0,16,16,3,3,32,32)
+end
 
+function draw_screen(xoff, yoff, w, h, screen_func)
+    local ox, oy = %0x5f28, %0x5f2a
+    clip(-ox+xoff,-oy+yoff,w,h)
+    camera(ox-xoff, oy-yoff)
+    rectfill(0,0,w,h,1)
+    screen_func()
+    camera(ox, oy)
+    clip()
+end
 
-    camera(0,-8)
-    -- memcpy(0x6000, 0x0000, 0x2000)
-    map(16, 16, -1,  0, 9, 16)
-
-    -- spr(96, 4, 10, 3, 1)
-
-    -- lights
-    local tval = t()*2\1%5 -- 0, 1, 2, 3, 4
-    spr((tval > 0) and 111 or 127, 20-1, 10)
-    spr((tval > 1) and 110 or 127, 15-1, 10)
-    spr((tval > 2) and 109 or 127, 10-1, 10)
-    spr((tval > 3) and 126 or 125, 3,  10)
+function draw_left_flap()
+    draw_screen(12, 29, 38, 38, draw_main_screen)
 
     map(0,  16, -1,  0, 8,  16)
-    map(8,  16, -1+8*8+2,-1, 8, 16)
-
-    -- button presses
-    spr(btn(0) and 186 or 154, 07, 88)
+    spr(btn(0) and 186 or 154, 07, 88) -- button presses
     spr(btn(1) and 188 or 156, 23, 88)
     spr(btn(2) and 171 or 139, 15, 80)
     spr(btn(3) and 187 or 155, 15, 88)
     spr(btn(4) and 170 or 138, 39, 80)
     spr(btn(5) and 172 or 140, 47, 88)
+end
 
-    -- small screen
-    spr(103, 77, 27, 5, 1)
-    -- zprint("\"gOTTA cATCH", 97, 40, 7, 0)
-    -- zprint("100 tYRANITAR", 97, 50, 7, 0)
+function draw_right_flap()
+    draw_screen(74, 24, 46, 14, function() zprint("wigglytuff", 46/2, 4, 7, 0) end)
+    draw_screen(74, 72, 14, 14, function() spr(78, 3, 3) end)
+    draw_screen(106, 72, 14, 14, function() spr(79, 3, 3) end)
 
-    -- credits
-    -- zprint("bY",                 97, 40+40, 7, 0)
+    map(8,  16, -1+8*8+2,-1, 8, 16)
+
     zprint("aMORG gAMES",           97, 41+50, 7, 0)
-    -- zprint("tHEGREATCADET",      97, 40+52, 7, 0)
+
+end
+
+function draw_back_panel()
+    map(16, 16, -1,  0, 9, 16)
+    local tval = t()*2\1%5 -- 0, 1, 2, 3, 4
+    spr((tval > 0) and 111 or 127, 20-1, 10)
+    spr((tval > 1) and 110 or 127, 15-1, 10)
+    spr((tval > 2) and 109 or 127, 10-1, 10)
+    spr((tval > 3) and 126 or 125, 3,  10)
+end
+
+function _draw()
+    cls()
+
+    -- draw the pokedex, use camera to change position
+    camera(0,-20)
+    draw_back_panel()
+    draw_left_flap()
+    draw_right_flap()
     camera()
 end
