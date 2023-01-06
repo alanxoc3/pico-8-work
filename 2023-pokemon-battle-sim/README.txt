c2a776e13bba7a1f163efaff21031ff02c3adc6d
Alan Morgan <alan@xoc3.io>
2023-01-05 20:58:13 -0700

    Working on many random things in parallel.

---
+++ 2023-pokemon-battle-sim/README.txt
@@ -0,0 +1,19 @@
+Blue buttons on the right correspond to the different pokedex modes.
+
+Modes are:
+- Mode Select
+- Credits
+- Settings (metric system or not)
+- Browse mode (by number)
+- Who's that pokemon?
+
+- Battle simulator
+- Browse mode (alphabetical)
+- Browse mode (by weight)
+- Browse mode (by height)
+- Party
+
+- Evolutions
+- Basic Info
+- Weight/Height
+- Locations on Map
--- 2023-pokemon-battle-sim/picodextest.lua
+++ 2023-pokemon-battle-sim/picodextest.lua
@@ -1,3 +1,32 @@
+-- ! means male. ? means female
+function wobble_text(text, x, y, color)
+    x -= #text*2
+
+    local t1 = ""
+    local t2 = ""
+    for i=1,#text do
+        letter = sub(text, i, i)
+        if letter == '!' then
+            letter = "\^:040e040e0e000000"
+        end
+
+        if letter == '?' then
+            letter = "\^:0e0e040e04000000"
+        end
+
+        if i%2 == 0 then
+            t1 = t1.." "
+            t2 = t2..letter
+        else
+            t1 = t1..letter
+            t2 = t2.." "
+        end
+    end
+
+    print(t1, x, y+(t()%2), color)
+    print(t2, x, y+((t()+1)%2), color)
+end
+
 function zprint(str, x, y, color, align)
     if align == 0    then x -= #str*2
     elseif align > 0 then x -= #str*4+1 end
@@ -8,25 +37,28 @@ end
 -- pixels are 0 to 37 (38x38 screen)
 function draw_main_screen()
     -- local fill_pattern = t()\1 % 2 == 0 and 0b1010010110100101 or 0b0101101001011010
-    local fill_pattern = t()*2\1 % 2 == 0 and 0b1111000011110000 or 0b0000111100001111
-    fillp(fill_pattern)
-    rectfill(0,0,37,37,0x5d)
-    fillp()
-    sspr(0,0,16,16,3,3,32,32)
+    -- local fill_pattern = t()*2\1 % 2 == 0 and 0b1111000011110000 or 0b0000111100001111
+    -- fillp(fill_pattern)
+    --rectfill(0,0,37,37,0x5d)
+    --fillp()
+    sspr(0,0,16,16,23,0+t()*2%2,16,16)
+    sspr(0,0,16,16,-1,21+(t()*2+1)%2,16,16, true)
 end
 
-function draw_screen(xoff, yoff, w, h, screen_func)
+function draw_screen(xoff, yoff, w, h, bg_color, screen_func)
     local ox, oy = %0x5f28, %0x5f2a
     clip(-ox+xoff,-oy+yoff,w,h)
     camera(ox-xoff, oy-yoff)
-    rectfill(0,0,w,h,1)
+
+    rectfill(0,0,w,h,   bg_color)
+
     screen_func()
     camera(ox, oy)
     clip()
 end
 
 function draw_left_flap()
-    draw_screen(12, 29, 38, 38, draw_main_screen)
+    draw_screen(12, 29, 38, 38, 5, draw_main_screen)
 
     map(0,  16, -1,  0, 8,  16)
     spr(btn(0) and 186 or 154, 07, 88) -- button presses
@@ -38,13 +70,16 @@ function draw_left_flap()
 end
 
 function draw_right_flap()
-    draw_screen(74, 24, 46, 14, function() zprint("wigglytuff", 46/2, 4, 7, 0) end)
-    draw_screen(74, 72, 14, 14, function() spr(78, 3, 3) end)
-    draw_screen(106, 72, 14, 14, function() spr(79, 3, 3) end)
+    draw_screen(74, 25, 46, 14,  5, function() wobble_text("nidoran?", 46/2, 4, 6) end)
+    draw_screen(74, 73, 46, 14,  5, function() sspr(32,32,16,16,1,1,12,12,true) end)
+    -- draw_screen(106, 73, 14, 14, 5, function() sspr(64,32,16,16,1,1,12,12,true) end)
 
-    map(8,  16, -1+8*8+2,-1, 8, 16)
+    map(8,  16, -1+8*8+2, 0, 8, 16)
 
-    zprint("aMORG gAMES",           97, 41+50, 7, 0)
+
+    -- spr(103, 77, 90, 5, 1)
+    spr(103, 77, 53, 5, 1)
+    --zprint("aMORG gAMES",           97, 41+50, 7, 0)
 
 end
 
