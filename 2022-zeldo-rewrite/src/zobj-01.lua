d6fa3bf1735dcb686c25770b369d41f0203f66b1
Alan Morgan <alan@xoc3.io>
2023-05-23 17:39:10 -0600

    Fixed mapbuilder too.

--- 2022-zeldo-rewrite/src/zobj-01.lua
+++ 2022-zeldo-rewrite/src/zobj-01.lua
@@ -46,7 +46,3 @@ end
 function f_zobj(...)
     return f_zobj_set({}, ...)
 end
-
--- set the initial state of _g. _g is needed for ztable "%" references to work.
--- see the perl preprocessor script for more info on G_TABLE_INITIALIZATION.
-_G_ZOBJ
